90372d0713116baf27b53a5d6c24f28ae74f54fd
RosaleeKnight <rtknight92@outlook.com>
2022-02-24 18:52:48 -0600

    Edit script.js

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ const board = $("<table>").attr("id", "board");
 const body = $(document.body);
 body.append(board);
 
-const hatOfMineLocations = [];
-for (let n = 0; n <= 80; n++) {
-    hatOfMineLocations.push(n);
-}
-
 const mineLocations = [];
 for (let i = 0; i < 10; i++) {
-    const mineLocationIndex = Math.floor(Math.random() * hatOfMineLocations.length);
-    const mineLocation = hatOfMineLocations[mineLocationIndex];
+    const mineLocation = Math.floor(Math.random() * 80);
     mineLocations.push(mineLocation);
-    hatOfMineLocations.splice(mineLocationIndex, 1);
 }
+
+const mines = [];
 
 for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
     const row = $("<tr>");
     board.append(row);
     for (let columnIndex = 0; columnIndex < 9; columnIndex++) {
-        const cell = $("<td>");
+        const space = $("<td>");
         const possibleMineLocation = (rowIndex * 9) + columnIndex;
-        if (mineLocations.indexOf(possibleMineLocation) !== -1) {
-            cell.addClass("mine");
+        const isMine = (mineLocations.indexOf(possibleMineLocation) !== -1);
+        if (isMine) {
+            mines.push(space);
         }
-        row.append(cell);
+        row.append(space);
+
+        space.on("click", () => {
+            if (isMine) {
+                mines.forEach(mine => {
+                    mine.addClass("mine");
+                });
+            }
+        }
     }
 }
