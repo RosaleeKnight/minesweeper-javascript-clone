272107042b5f27718ea0095a54b5f46dea57bb1a
RosaleeKnight <rtknight92@outlook.com>
2022-02-24 19:25:10 -0600

    Edit style.css and script.js

--- script.js
+++ script.js
@@ -9,25 +9,60 @@ for (let i = 0; i < 10; i++) {
 }
 
 const mines = [];
+const spaces = [];
 
 for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
     const row = $("<tr>");
+
     board.append(row);
+    spaces[rowIndex] = [];
+
     for (let columnIndex = 0; columnIndex < 9; columnIndex++) {
         const space = $("<td>");
         const possibleMineLocation = (rowIndex * 9) + columnIndex;
         const isMine = (mineLocations.indexOf(possibleMineLocation) !== -1);
+ 
         if (isMine) {
             mines.push(space);
+            space.addClass("mine");
+            space.data("isMine", true);
         }
-        row.append(space);
 
+        spaces[rowIndex][columnIndex] = space;
+ 
+        row.append(space);
+ 
         space.on("click", () => {
             if (isMine) {
                 mines.forEach(mine => {
-                    mine.addClass("mine");
+                    mine.addClass("uncovered");
                 });
+            } else {
+                const previousRow = spaces[rowIndex - 1] || [];
+                const nextRow = spaces[rowIndex + 1] || [];
+                const currentRow = spaces[rowIndex];
+                const neighborsWithPossibleNulls = [
+                    currentRow[columnIndex - 1],
+                    currentRow[columnIndex + 1],
+                    previousRow[columnIndex - 1],
+                    previousRow[columnIndex],
+                    previousRow[columnIndex + 1],
+                    nextRow[columnIndex - 1],
+                    nextRow[columnIndex],
+                    nextRow[columnIndex + 1]
+                ];
+                const neighbors = neighborsWithPossibleNulls.filter(neighbor => {
+                    return neighbor != null;
+                });
+                const neighboringMines = neighbors.filter(neighbor => {
+                    return neighbor.data("isMine");
+                });
+                const numNeighboringMines = neighboringMines.length;
+                if (numNeighboringMines > 0) {
+                    space.addClass("uncovered");
+                    space.text(numNeighboringMines);
+                }
             }
-        }
+        });
     }
-}
+  } 
